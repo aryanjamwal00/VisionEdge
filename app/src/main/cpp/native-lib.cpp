dde283b54fa6e62f275e95452451cbd8d6156995
aryan <aryanjamwal1906@gmail.com>
2025-11-22 23:53:58 +0530

    chore: initial Android Studio native-cpp project

--- app/src/main/cpp/native-lib.cpp
+++ app/src/main/cpp/native-lib.cpp
@@ -1,10 +1,11 @@
+#include <android/log.h>
 #include <jni.h>
-#include <string>
+
+#define LOG_TAG "VisionEdgeNative"
+#define LOGI(...) __android_log_print(ANDROID_LOG_INFO, LOG_TAG, __VA_ARGS__)
 
 extern "C" JNIEXPORT jstring JNICALL
-Java_com_example_visionedge_MainActivity_stringFromJNI(
-        JNIEnv* env,
-        jobject /* this */) {
-    std::string hello = "Hello from C++";
-    return env->NewStringUTF(hello.c_str());
+Java_com_example_visionedge_MainActivity_stringFromJNI(JNIEnv* env, jobject) {
+    LOGI("VisionEdge native module initialized successfully!");
+    return env->NewStringUTF("VisionEdge native module initialized");
 }
